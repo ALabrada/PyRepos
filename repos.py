258bd40aa648e9d1a70b1a6f154e05aa2c9a1bbc
Alexei Labrada <alexei.labrada@cneuro.edu.cu>
2019-06-12 11:11:07 +0300

    Remove isolated repositories from the graph.

--- repos.py
+++ repos.py
@@ -76,24 +76,42 @@ class GithubCrawler:
         return g
 
 
-def analize_graph(g, limit=3):
+def analize_graph(g, limit=3, clean=True):
+    def take_by_value(items, l, f=None):
+        items = sorted(items, key=lambda t: -t[1])
+        if f is not None:
+            items = filter(f, items)
+        return [k for k, v in itertools.islice(items, 0, l)]
+
     nodes = g.nodes(data=True)
     repos = {n for n, d in nodes if d['bipartite'] == 0}
     print('Repositories: {0}'.format(len(repos)))
     users = set(g) - repos
     print('Users: {0}'.format(len(users)))
-    print('Connected components: \n{0}'.format(sum(1 for _ in nx.connected_components(g))))
+    components = list(nx.connected_components(g))
+    print('Connected components: \n{0}'.format(sum(1 for _ in components)))
+
+    if clean:
+        repo_count = len(repos)
+        for component in components:
+            component_repos = repos.intersection(component)
+            if len(component_repos) == 1:
+                repos.difference_update(component)
+                users.difference_update(component)
+                # g.remove_nodes_from(component)
+
+        if len(repos) < repo_count:
+            print('Excluded {0} isolated projects.'.format(repo_count - len(repos)))
+            g = nx.classes.graphviews.subgraph_view(g, filter_node=lambda n: n in repos or n in users)
+
+    bridges = {(n1, n2): len(next((c for c in components if n1 in c), [])) for n1, n2 in nx.algorithms.bridges(g)}
+    bridges = take_by_value(bridges.items(), limit)
+    print('Connecting memberships: \n{0}'.format(list(bridges)))
 
     deg1_repos = [n for n in repos if g.degree[n] <= 1]
     print('Number of risked projects: \n{0}'.format(len(deg1_repos)))
     deg1_repos = sorted(deg1_repos, key=lambda n: -nodes[n].get('downloads', 0))
     print('Most risked projects: \n{0}'.format(deg1_repos[0:limit]))
-
-    def take_by_value(items, l, f=None):
-        items = sorted(items, key=lambda t: -t[1])
-        if f is not None:
-            items = filter(f, items)
-        return dict(itertools.islice(items, 0, l))
 
     repo_centrality = nx.algorithms.bipartite.degree_centrality(g, repos)
     repo_centrality = take_by_value(repo_centrality.items(), limit, f=lambda t: t[0] in repos)
@@ -114,9 +132,6 @@ def analize_graph(g, limit=3):
     user_centrality = nx.algorithms.bipartite.betweenness_centrality(g, users)
     user_centrality = take_by_value(user_centrality.items(), limit, f=lambda t: t[0] in users)
     print('Most connecting users: \n{0}'.format(user_centrality))
-
-    bridges = nx.algorithms.bridges(g)
-    print('Connecting memberships: \n{0}'.format(list(bridges)))
 
 
 if __name__ == "__main__":
@@ -143,6 +158,6 @@ if __name__ == "__main__":
     g = None if args.input is None else nx.read_gexf(args.input)
     if nx.number_of_nodes(g) == 0 or args.scan:
         g = c.find(args.query, limit=args.limit, since=args.date, previous=g)
+        if args.output:
+            nx.write_gexf(g, args.output)
     analize_graph(g)
-    if args.output:
-        nx.write_gexf(g, args.output)
