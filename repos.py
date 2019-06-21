0310e80a4164054ce69452603bf99351957882e7
Alexei Labrada <alexei.labrada@cneuro.edu.cu>
2019-06-21 12:58:20 +0300

    Improve drawing, showing labels for the important nodes.

--- repos.py
+++ repos.py
@@ -8,10 +8,9 @@ from matplotlib import cm, colors
 from GithubCrawler import GithubCrawler
 
 
-def analize_graph(g: nx.Graph, limit: int = 3, clean: bool = True):
+def analize_graph(g: nx.Graph, limit: int = 3, clean: bool = True, draw: bool = False):
     assert isinstance(g, nx.Graph)
-    assert isinstance(limit, int) and limit >= 0
-    assert isinstance(clean, bool)
+    assert isinstance(limit, int)
 
     def take_by_value(items, l, f=None):
         items = sorted(items, key=lambda t: -t[1])
@@ -32,69 +31,76 @@ def analize_graph(g: nx.Graph, limit: int = 3, clean: bool = True):
         repo_count = len(repos)
         for component in components:
             component_repos = repos.intersection(component)
-            if len(component_repos) == 1:
+            if len(component_repos) <= 5:
                 repos.difference_update(component)
                 users.difference_update(component)
                 # g.remove_nodes_from(component)
 
         if len(repos) < repo_count:
             print('Excluded {0} isolated projects.'.format(repo_count - len(repos)))
-            g = nx.classes.graphviews.subgraph_view(g, filter_node=lambda n: n in repos or n in users)
+            # g = nx.classes.graphviews.subgraph_view(g, filter_node=lambda n: n in repos or n in users)
+            g = nx.subgraph(g, repos.union(users))
 
-    bridges = {(n1, n2): len(next((c for c in components if n1 in c), [])) for n1, n2 in nx.algorithms.bridges(g)}
-    bridges = take_by_value(bridges.items(), limit)
-    print('Connecting memberships: \n{0}'.format(list(bridges)))
+    labels = set()
+    if limit:
+        bridges = {(n1, n2): len(next((c for c in components if n1 in c), [])) for n1, n2 in nx.algorithms.bridges(g)}
+        bridges = take_by_value(bridges.items(), limit)
+        print('Connecting memberships: \n{0}'.format(list(bridges)))
 
-    deg1_repos = [n for n in repos if g.degree[n] <= 1]
-    print('Number of risked projects: \n{0}'.format(len(deg1_repos)))
-    deg1_repos = sorted(deg1_repos, key=lambda n: -nodes[n].get('weight', 0))
-    print('Most risked projects: \n{0}'.format(deg1_repos[0:limit]))
+        deg1_repos = [n for n in repos if g.degree[n] <= 1]
+        print('Number of risked projects: \n{0}'.format(len(deg1_repos)))
+        deg1_repos = sorted(deg1_repos, key=lambda n: -nodes[n].get('weight', 0))
+        print('Most risked projects: \n{0}'.format(deg1_repos[0:limit]))
 
-    repo_centrality = nx.algorithms.bipartite.degree_centrality(g, repos)
-    repo_centrality = take_by_value(repo_centrality.items(), limit, f=lambda t: t[0] in repos)
-    print('Most popular projects: \n{0}'.format(repo_centrality))
+        repo_centrality = nx.algorithms.bipartite.degree_centrality(g, repos)
+        repo_centrality = take_by_value(repo_centrality.items(), limit, f=lambda t: t[0] in repos)
+        labels.update(repo_centrality)
+        print('Most popular projects: \n{0}'.format(repo_centrality))
 
-    repo_centrality = nx.algorithms.bipartite.closeness_centrality(g, repos, normalized=True)
-    repo_centrality = take_by_value(repo_centrality.items(), limit, f=lambda t: t[0] in repos)
-    print('Most relatable projects: \n{0}'.format(repo_centrality))
+        repo_centrality = nx.algorithms.bipartite.closeness_centrality(g, repos, normalized=True)
+        repo_centrality = take_by_value(repo_centrality.items(), limit, f=lambda t: t[0] in repos)
+        labels.update(repo_centrality)
+        print('Most relatable projects: \n{0}'.format(repo_centrality))
 
-    repo_centrality = nx.algorithms.bipartite.betweenness_centrality(g, repos)
-    repo_centrality = take_by_value(repo_centrality.items(), limit, f=lambda t: t[0] in repos)
-    print('Most connecting projects: \n{0}'.format(repo_centrality))
+        repo_centrality = nx.algorithms.bipartite.betweenness_centrality(g, repos)
+        repo_centrality = take_by_value(repo_centrality.items(), limit, f=lambda t: t[0] in repos)
+        labels.update(repo_centrality)
+        print('Most connecting projects: \n{0}'.format(repo_centrality))
 
-    user_centrality = nx.algorithms.bipartite.degree_centrality(g, users)
-    user_centrality = take_by_value(user_centrality.items(), limit, f=lambda t: t[0] in users)
-    print('Most active users: \n{0}'.format(user_centrality))
+        user_centrality = nx.algorithms.bipartite.degree_centrality(g, users)
+        user_centrality = take_by_value(user_centrality.items(), limit, f=lambda t: t[0] in users)
+        labels.update(user_centrality)
+        print('Most active users: \n{0}'.format(user_centrality))
 
-    user_centrality = nx.algorithms.bipartite.betweenness_centrality(g, users)
-    user_centrality = take_by_value(user_centrality.items(), limit, f=lambda t: t[0] in users)
-    print('Most connecting users: \n{0}'.format(user_centrality))
+        user_centrality = nx.algorithms.bipartite.betweenness_centrality(g, users)
+        user_centrality = take_by_value(user_centrality.items(), limit, f=lambda t: t[0] in users)
+        labels.update(user_centrality)
+        print('Most connecting users: \n{0}'.format(user_centrality))
+
+    if draw:
+        draw_communities(g, labels=list(labels))
 
 
-def draw_communities(G: nx.Graph):
+def draw_communities(G: nx.Graph, labels=None):
+    assert isinstance(G, nx.Graph)
+    assert labels is None or isinstance(labels, list)
+
+    labels = {n: n for n in labels} if labels else {}
+
     print('Drawing graph...')
     pos = nx.spring_layout(G)
-    nodes = g.nodes(data=True)
+    nodes = G.nodes(data=True)
     repos = {n for n, d in nodes if d['bipartite'] == 0}
-    users = set(g) - repos
-    groups = {0: repos, 1: users}
+    users = set(G) - repos
+    norm = colors.Normalize(vmin=0, vmax=2)
+    node_list = list(G.nodes)
+    color_list = [cm.jet(norm(d['bipartite'])) for n, d in nodes]
+    size_list = [1 if d['bipartite'] else 5 for n, d in nodes]
 
     fig, ax = plt.subplots(figsize=(16, 9))
-
-    # Normalize number of clubs for choosing a color
-    norm = colors.Normalize(vmin=0, vmax=2)
-
-    for group, members in groups.items():
-        nx.draw_networkx_nodes(G, pos,
-                               nodelist=members,
-                               node_color=cm.jet(norm(group)),
-                               node_size=5,
-                               alpha=0.8,
-                               ax=ax)
-
-    # Draw edges (social connections) and show final plot
     plt.title("Github repositories")
-    nx.draw_networkx_edges(G, pos, alpha=0.5, ax=ax)
+    nx.draw_networkx(G, pos=pos, nodelist=node_list, node_color=color_list, node_size=size_list,
+                     with_labels=True, labels=labels, ax=ax, edge_color='0.5')
     plt.show()
 
 
@@ -104,7 +110,8 @@ if __name__ == "__main__":
     parser.add_argument('-c', '--continue', dest='scan', action='store_true',
                         help='Include more repositories from the search results. '
                              'It is ignored if there is no input graph.')
-    parser.add_argument('--no-stats', dest='no_stats', action='store_true', help='Avoid analyzing graph statistics.')
+    parser.add_argument('--stats', type=int, help='Specify the amount of results to display in graph analysis. '
+                                        'Use 0 to disable graph analysis.')
     parser.add_argument('--draw', dest='draw', action='store_true', help='Draw the resulting graph.')
     parser.add_argument('-s', '--source', default='GitHub', choices=['GitHub', 'GitLab'],
                         help='The type of repository.')
@@ -127,12 +134,6 @@ if __name__ == "__main__":
         for g in c.find(args.query, limit=args.limit, since=args.date, previous=g):
             if args.output:
                 nx.write_gexf(g, args.output)
-            if not args.no_stats:
-                analize_graph(g)
-            if args.draw:
-                draw_communities(g)
+            analize_graph(g, limit=args.stats, draw=args.draw)
     else:
-        if not args.no_stats:
-            analize_graph(g)
-        if args.draw:
-            draw_communities(g)
+        analize_graph(g, limit=args.stats, draw=args.draw)
